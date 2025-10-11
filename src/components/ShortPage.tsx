53afce2da27af740e15f97d43630d65295334a34
Aregus <74340447+Areg472@users.noreply.github.com>
2025-10-11 22:21:05 +0400

    Hey GPT. Generate an image of an idiot who puts a body after a main tag in Next JS
    https://gr5mutu1hr.ufs.sh/f/thKihuQxhYcPGNE6ZVCLfYs1lZdjQAk7ImvXKcSGpgPRNO4W

--- src/components/ShortPage.tsx
+++ src/components/ShortPage.tsx
@@ -12,7 +12,7 @@ export function ShortPage({
 }) {
   return (
     <>
-      <body>
+      <div>
         <div className="px-4 md:px-6 lg:px-8">
           <h1 className="issue text-center leading-normal lg:text-left">
             {title}
@@ -38,7 +38,7 @@ export function ShortPage({
             </div>
           </div>
         </div>
-      </body>
+      </div>
     </>
   );
 }
