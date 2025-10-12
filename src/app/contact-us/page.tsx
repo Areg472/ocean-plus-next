587273a9d6ec4de46086e5afecde7a3fae9f23e9
Aregus <74340447+Areg472@users.noreply.github.com>
2025-10-12 12:18:31 +0400

    privacy policy page

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,4 +1,14 @@
 import ContactUsPage from "@/components/ContactUsPage";
+import type { Metadata } from "next";
+import { createMetadata } from "@/lib/metadata";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const title = "Ocean+ - Contact Us";
+  const description = `Contact to the Ocean+ team and get support or request your content to be added!`;
+  const pathname = `/contact-us`;
+
+  return createMetadata({ title, description, pathname });
+}
 
 export default function Contactus() {
   return <ContactUsPage />;
--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,5 +1,7 @@
 import { createMetadata } from "@/lib/metadata";
 import type { Metadata } from "next";
+import { Footer } from "@/components/footer";
+import { Navbar } from "@/components/navbar";
 
 export async function generateMetadata(): Promise<Metadata> {
   const title = "Ocean+ - Privacy Policy";
@@ -10,5 +12,29 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 export default function PrivacyPolicy() {
-  return <div>Privacy</div>;
+  return (
+    <>
+      <div className="min-h-screen flex flex-col justify-between">
+        <Navbar />
+        <div className="flex-1 flex items-center justify-center py-8">
+          <div className="container text-center">
+            <p className="text-xl leading-loose">
+              By using Ocean+ your session data will be collected by Heap. We
+              dont
+              <br /> sell your data to third party companies. By visiting any
+              Ocean+ page your request info,
+              <br /> Network service provider, Postal code(in some countries),
+              User Agent,
+              <br /> IP address and request codes will be stored on a logging
+              service for 3 days before deleted.
+              <br /> By visiting anything on Ocean+ for more than 30 seconds
+              <br />
+              then your session will be recorded by using Hotjar.
+            </p>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
 }
