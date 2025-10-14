f84c19eceec6cee8288ec645443434b17cec4c1e
Aregus <74340447+Areg472@users.noreply.github.com>
2025-10-14 11:45:26 +0400

    shortening of contact us too, everything is now server side besides the form

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,6 +1,9 @@
-import ContactUsPage from "@/components/ContactUsPage";
 import type { Metadata } from "next";
 import { createMetadata } from "@/lib/metadata";
+import { Navbar } from "@/components/navbar";
+import { Footer } from "@/components/footer";
+import "@/components/contactus.css";
+import ContactForm from "@/components/ContactForm";
 
 export const metadata: Metadata = createMetadata({
   title: "Ocean+ - Contact Us",
@@ -10,5 +13,22 @@ export const metadata: Metadata = createMetadata({
 });
 
 export default function Contactus() {
-  return <ContactUsPage />;
+  return (
+    <>
+      <div className="flex min-h-screen flex-col">
+        <Navbar />
+        <div className="flex flex-1 items-center justify-center px-4 py-8 sm:px-6 lg:px-8">
+          <div className="contact mx-auto w-full max-w-md text-center">
+            <h1>Contact Us</h1>
+            <h2 className="mt-2.5">
+              Contact us for movies by filling out the form below. If you have
+              any other stuff, email us at oceanplus@oceanbluestream.com
+            </h2>
+            <ContactForm />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
 }
---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import Turnstile, { useTurnstile } from "react-turnstile";
+import { useLogger } from "@logtail/next";
+import "./contactus.css";
+
+declare global {
+  interface Window {
+    heap: {
+      identify: (uniqueId: string, properties?: object) => void;
+      addUserProperties: (properties: object) => void;
+    };
+  }
+}
+
+export default function ContactForm() {
+  const turnstile = useTurnstile();
+  const log = useLogger();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get("email") as string;
+    const name = formData.get("name") as string;
+    const message = formData.get("message") as string;
+
+    log.debug("Contact form submitted", { name, email, message });
+
+    if (typeof window.heap !== "undefined") {
+      window.heap.identify(name);
+
+      window.heap.addUserProperties({
+        name: name,
+        email: email,
+      });
+    }
+
+    e.currentTarget.submit();
+  };
+
+  const formID = process.env.NEXT_PUBLIC_FORM_ID;
+  const CFSiteKey = process.env.NEXT_PUBLIC_CF_TURNSTILE_SITE_KEY;
+
+  if (!CFSiteKey) {
+    console.error("Missing PUBLIC_CF_TURNSTILE_SITE_KEY environment variable");
+  }
+  if (!formID) {
+    console.error("Missing PUBLIC_FORM_ID environment variable");
+  }
+
+  return (
+    <>
+      <form
+        onSubmit={handleSubmit}
+        action={`https://submit-form.com/${formID}`}
+        className="form"
+      >
+        <label htmlFor="name" className="nametext">
+          Name
+        </label>
+        <br />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Name"
+          className="field otherfield rounded-md"
+          required={true}
+        />
+        <br />
+        <label htmlFor="email" className="emailtext">
+          Email
+        </label>
+        <br />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email"
+          className="field otherfield rounded-md"
+          required={true}
+        />
+        <br />
+        <label htmlFor="message" className="movietext">
+          Movie link
+        </label>
+        <br />
+        <textarea
+          id="message"
+          name="message"
+          placeholder="Message"
+          className="field textarea mb-2.5 rounded-md"
+          required={true}
+        ></textarea>
+        <br />
+        <Turnstile
+          sitekey={CFSiteKey}
+          onVerify={(token) => {
+            fetch("/Contact-Us", {
+              method: "POST",
+              body: JSON.stringify({ token }),
+            }).then((response) => {
+              if (!response.ok)
+                turnstile.return("Turnstile token successfuly validated.");
+            });
+          }}
+        />
+        <button type="submit" className="submit cursor-pointer">
+          Send
+        </button>
+      </form>
+    </>
+  );
+}
